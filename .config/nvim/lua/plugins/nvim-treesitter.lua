ef04b5b2dc54309ea9ba16d7e997d9504346e4dd
thedvd98 <test@test>
2024-12-01 09:25:13 +0100

    Bugfix treesitter config not loading correctly

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,37 +1,45 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-    opts = {
-        ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc'},
-        sync_install = false,
-        auto_install = false,
-        highlight = {
-            -- `false` will disable the whole extension
-            enable = true,
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'yaml', 'typescript', 'javascript', 'ocaml'},
+            auto_install = true,
+            sync_install = false,
+            ignore_install = { 'ruby' },
+            modules = {},
+            highlight = {
+                enable = true,
 
-            -- disable = function(lang, bufnr) -- Disable in large C++ buffers
-            --     return lang == "cpp" and vim.api.nvim_buf_line_count(bufnr) > 500000
-            -- end,
----@diagnostic disable-next-line: unused-local
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
----@diagnostic disable-next-line: undefined-field
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-            -- list of language that will be disabled
-            -- disable = { "c", "rust" },
+                -- disable = function(lang, bufnr) -- Disable in large C++ buffers
+                    --     return lang == "cpp" and vim.api.nvim_buf_line_count(bufnr) > 500000
+                    -- end,
+                    disable = function(_, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
+                    -- list of language that will be disabled
+                    -- disable = { "c", "rust" },
 
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
-        -- TODO Check what these two does
-        -- incremental_selection = { enable = true },
-        -- textobjects = { enable = true },
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+            })
+        end
     }
-}

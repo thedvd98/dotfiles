d7e7f1c869f30506f257a83d5abf8f2d4ce4eadc
thedvd98 <thedvd98>
2025-01-30 21:45:36 +0100

    screenshots with maim

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -2,6 +2,6 @@
 
 case $(hostname) in
     "vxwin") scrot "%Y-%m-%d_%H-%M-%S.png" -s -e 'mv $f ~/img/screenshots/';;
-    "segfault") scrot -s ~/Pictures/screenshots/screenshot_%Y-%m-%d-%T.png;;
+    "segfault") maim -s ~/Pictures/screenshots/screenshot_$(date +'%Y-%m-%d-%T').png;;
 esac
 
